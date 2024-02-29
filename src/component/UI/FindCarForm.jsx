5e3a38f96029080c9d868aac9e0c3d5d0bae9bb7
MIHIRANGA S.S <sachinmihiranga2000@gmail.com>
2024-02-29 21:52:44 +0530

    Find car form add to home page

---
+++ src/component/UI/FindCarForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../styles/find-car-form.css'
+import '../../styles/find-car-form.css'
+
+
+const FindCarForm = () => {
+  return (
+    <div>
+      FindCarForm
+    </div>
+  )
+}
+
+export default FindCarForm
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,32 @@
  import React from 'react'
  import HeroSlider from '../component/UI/HeroSlider'
  import Helmet from '../component/Helmet/Helmet'
+
+import { Container, Row, Col } from 'reactstrap'
+import FindCarForm from '../component/UI/FindCarForm'
+
  const Home = () => {
 
    return (
      <Helmet title='Home'>
+
+      {/* ========== hero section ========== */}
       <section className='p-0 hero_slider-section'>
         <HeroSlider />
+        <div className="hero_form">
+          <Container>
+            <Row className='form_row'>
+               <Col lg='4' md='4'>
+                 <div className='find_cars-left'>
+                  <h2>Find your best car here</h2> 
+                 </div>
+               </Col>
+               <Col lg='8' md='8' sm='12'>
+                  <FindCarForm/>
+               </Col>
+            </Row>
+          </Container>
+        </div>
       </section>
    </Helmet>
    );
