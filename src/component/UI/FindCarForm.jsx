840a269828ca2445c9eb692f20da99ff89e95d27
MIHIRANGA S.S <sachinmihiranga2000@gmail.com>
2024-03-07 01:16:31 +0530

    add form to home page

--- src/component/UI/FindCarForm.jsx
+++ src/component/UI/FindCarForm.jsx
@@ -1,14 +1,43 @@
 import React from 'react'
 import '../../styles/find-car-form.css'
 import '../../styles/find-car-form.css'
-
+import { Form, FormGroup } from 'reactstrap'  
 
 const FindCarForm = () => {
   return (
-    <div>
-      FindCarForm
-    </div>
+  <Form className='form'>
+      <div className='d-flex align-items-center justify-content-between
+       flex-wrap'>
+         <FormGroup className='form_group'>
+           <input type='text' placeholder='From address' required />
+         </FormGroup>
+
+         <FormGroup className='form_group'>
+           <input type='text' placeholder='To address' required />
+         </FormGroup>
+
+         <FormGroup className='form_group'>
+           <input type='date' placeholder='Journey date' required />
+         </FormGroup>
+
+         <FormGroup className='form_group'>
+           <input type='journey_time' placeholder='Journey time' required />
+         </FormGroup>
+        <FormGroup className='select_group'> 
+         <select>
+              <option value="ac">AC Car</option>
+              <option value="non-ac">Non AC Car</option>
+         </select>
+         </FormGroup>
+
+         <FormGroup className='form_group'>
+          <button className=' find_car-btn'>Find Car</button>
+         </FormGroup>
+       </div>
+  </Form>
   )
+    
+  
 }
 
 export default FindCarForm
